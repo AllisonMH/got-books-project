bd465c3d912bf67730d3376d366ea178e143f8b5
allisonmh <12721227+AllisonMH@users.noreply.github.com>
2021-01-29 13:46:40 -0500

    Updated code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App () {
+  const [books, setBooks] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get('https://www.anapioficeandfire.com/api/books?pageSize=30');
+    setBooks(response.data);
+  };
+  console.log("Books ", books);
+
+  return (
+    <div className = 'App'>
+      <h1>GOT BOOKS</h1>
+      <div>
+        <button className='fetch-button' onClick={fetchData}>Fetch Data</button>
+        <br />
+      </div>
+
+      <div className='books'>
+        {books &&
+          books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(', ');
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+                <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages} pages</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
